8febbc1e812aa66cdd882f4e8a92177fba5df429
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2025-10-31 18:17:23 -0400

    Resolve console errors and refactor AJAX functions to async await.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var settings = {
 let myAudio = new Audio();
 let currentSongNumber = 0;
 let arrayOfAllDisplayedSongs = [];
+let searchButton = document.getElementById("searchButton");
 let currentSongContainer = document.getElementsByClassName("songContainer");
 const playButton = document.getElementById("playButton");
 const pauseButton = document.getElementById("pauseButton");
@@ -27,8 +28,6 @@ function removeActiveSongContainerStyling() {
 }
 
 // Search for an artist with a button click event
-let searchButton = document.getElementById("searchButton");
-
 searchButton.addEventListener("click", async () => {
 	currentSongNumber = 0;
 	let artistName = document.getElementById("artistName").value;
@@ -40,8 +39,7 @@ searchButton.addEventListener("click", async () => {
 	const response = await dataResponse.json();
 
 	if (artistName !== "") {
-		// clear previous search results
-		searchResultsContainer.innerText = "";
+		searchResultsContainer.innerText = ""; // clear previous search results
 
 		for (let i = 0; i < response.data.length; i++) {
 			const songContainer = document.createElement("DIV");
@@ -59,22 +57,28 @@ searchButton.addEventListener("click", async () => {
 		}
 		myAudio.src = response.data[currentSongNumber].preview; // set audio src to first track
 		$("#currentSongField").val("Track " + (currentSongNumber + 1) + ": " + response.data[currentSongNumber].title); // show current song
-		$(".button").removeClass("activeButton");
+		$("#pauseButton").addClass("activeButton");
+		$("#playButton").removeClass("activeButton");
 	}
 });
 
 // Play a song with a button click event
-playButton.addEventListener("click", function () {
-	myAudio.play();
-	$(".button").removeClass("activeButton"); // reset button colors and make play button dark
-	$("#playButton").addClass("activeButton");
-	removeActiveSongContainerStyling();
-	currentSongContainer[currentSongNumber].classList.add("activeSongContainer");
+playButton.addEventListener("click", () => {
+	if (currentSongContainer.length !== 0) {
+		myAudio.play();
+		$("#pauseButton").removeClass("activeButton"); // reset button colors and make play button dark
+		$("#playButton").addClass("activeButton");
+		removeActiveSongContainerStyling();
+		currentSongContainer[currentSongNumber].classList.add("activeSongContainer");
+	}
 });
 
 // Go to next song with a button click event
-nextButton.addEventListener("click", function () {
-	$.ajax(settings).done(function (response) {
+nextButton.addEventListener("click", async () => {
+	const dataResponse = await fetch(settings.url, settings);
+	const response = await dataResponse.json();
+
+	if (currentSongContainer.length !== 0) {
 		if (currentSongNumber !== response.data.length - 1) {
 			// If not at last song...
 			currentSongNumber += 1;
@@ -85,16 +89,19 @@ nextButton.addEventListener("click", function () {
 		myAudio.src = response.data[currentSongNumber].preview;
 		myAudio.play();
 		$("#currentSongField").val("Track " + (currentSongNumber + 1) + ": " + response.data[currentSongNumber].title);
-		$(".button").removeClass("activeButton");
+		$("#pauseButton").removeClass("activeButton");
 		$("#playButton").addClass("activeButton");
 		removeActiveSongContainerStyling();
 		currentSongContainer[currentSongNumber].classList.add("activeSongContainer");
-	});
+	}
 });
 
 // Go back to previous song with a button click event
-previousButton.addEventListener("click", function () {
-	$.ajax(settings).done(function (response) {
+previousButton.addEventListener("click", async () => {
+	const dataResponse = await fetch(settings.url, settings);
+	const response = await dataResponse.json();
+
+	if (currentSongContainer.length !== 0) {
 		if (currentSongNumber > 0) {
 			currentSongNumber -= 1;
 		} else {
@@ -103,58 +110,62 @@ previousButton.addEventListener("click", function () {
 		myAudio.src = response.data[currentSongNumber].preview;
 		myAudio.play();
 		$("#currentSongField").val("Track " + (currentSongNumber + 1) + ": " + response.data[currentSongNumber].title);
-		$(".button").removeClass("activeButton");
+		$("#pauseButton").removeClass("activeButton");
 		$("#playButton").addClass("activeButton");
 		removeActiveSongContainerStyling();
 		currentSongContainer[currentSongNumber].classList.add("activeSongContainer");
-	});
+	}
 });
 
 // Pause song with a button click event
-pauseButton.addEventListener("click", function () {
-	myAudio.pause();
-	$(".button").removeClass("activeButton");
-	$("#pauseButton").addClass("activeButton");
+pauseButton.addEventListener("click", () => {
+	if (currentSongContainer.length !== 0) {
+		myAudio.pause();
+		$("#playButton").removeClass("activeButton");
+		$("#pauseButton").addClass("activeButton");
+	}
 });
 
 // Automatically go to the next song when current song ends
-myAudio.onended = function () {
-	$.ajax(settings).done(function (response) {
-		if (currentSongNumber !== response.data.length - 1) {
-			currentSongNumber += 1;
-		} else {
-			currentSongNumber = 0;
-		}
-		myAudio.src = response.data[currentSongNumber].preview;
-		myAudio.play();
-		$("#currentSongField").val("Track " + (currentSongNumber + 1) + ": " + response.data[currentSongNumber].title);
-		$(".button").removeClass("activeButton");
-		$("#playButton").addClass("activeButton");
-		removeActiveSongContainerStyling();
-		currentSongContainer[currentSongNumber].classList.add("activeSongContainer");
-	});
+myAudio.onended = async () => {
+	const dataResponse = await fetch(settings.url, settings);
+	const response = await dataResponse.json();
+
+	if (currentSongNumber !== response.data.length - 1) {
+		currentSongNumber += 1;
+	} else {
+		currentSongNumber = 0;
+	}
+	myAudio.src = response.data[currentSongNumber].preview;
+	myAudio.play();
+	$("#currentSongField").val("Track " + (currentSongNumber + 1) + ": " + response.data[currentSongNumber].title);
+	$("#pauseButton").removeClass("activeButton");
+	$("#playButton").addClass("activeButton");
+	removeActiveSongContainerStyling();
+	currentSongContainer[currentSongNumber].classList.add("activeSongContainer");
 };
 
 // Play song when user clicks an album song thumbnail
 document.addEventListener("click", selectAndPlayClickedSong);
 document.addEventListener("keydown", selectAndPlayClickedSong);
 
-function selectAndPlayClickedSong(event) {
+async function selectAndPlayClickedSong(event) {
 	if (
 		event.target.closest(".songContainer") &&
 		(event.type === "click" || (event.type === "keydown" && event.key === "Enter"))
 	) {
 		arrayOfAllDisplayedSongs = [...document.getElementsByClassName("songContainer")];
 		indexOfClickedSong = arrayOfAllDisplayedSongs.indexOf(event.target.closest(".songContainer"));
-		$.ajax(settings).done(function (response) {
-			currentSongNumber = indexOfClickedSong;
-			myAudio.src = response.data[currentSongNumber].preview; // set audio src to first track
-			$("#currentSongField").val("Track " + (currentSongNumber + 1) + ": " + response.data[currentSongNumber].title); // show current song
-			myAudio.play();
-			$(".button").removeClass("activeButton"); // reset button colors and make play button dark
-			$("#playButton").addClass("activeButton");
-			removeActiveSongContainerStyling();
-			arrayOfAllDisplayedSongs[indexOfClickedSong].classList.add("activeSongContainer");
-		});
+		const dataResponse = await fetch(settings.url, settings);
+		const response = await dataResponse.json();
+
+		currentSongNumber = indexOfClickedSong;
+		myAudio.src = response.data[currentSongNumber].preview; // set audio src to first track
+		$("#currentSongField").val("Track " + (currentSongNumber + 1) + ": " + response.data[currentSongNumber].title); // show current song
+		myAudio.play();
+		$("#pauseButton").removeClass("activeButton"); // reset button colors and make play button dark
+		$("#playButton").addClass("activeButton");
+		removeActiveSongContainerStyling();
+		arrayOfAllDisplayedSongs[indexOfClickedSong].classList.add("activeSongContainer");
 	}
 }
