3eab15489a6fdeec620e3331375fbe6d1d5800b4
Thomas Cooper <thomascooper@Thomass-MacBook-Pro.local>
2025-12-18 22:28:20 -0500

    Adjust conditional to check for array length because arrays are always truthy.

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ document.addEventListener("keydown", selectAndPlayClickedSong);
 
 function selectAndPlayClickedSong(event) {
 	// If songs haven't been fetched, do not continue...
-	if (!cachedSongs && !customPlayList) {
+	if (!cachedSongs.length && !customPlayList.length) {
 		return;
 	}
 	// If user is clicking "Add to playlist" button, don't play the song...
